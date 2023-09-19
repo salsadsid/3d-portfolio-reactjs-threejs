3b12ad99e98e661e09e2ea966fdd5218d2d89f4c
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-09-19 14:24:33 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { styles } from '../styles'
+import { Link } from 'react-router-dom'
+import {logo,menu,close} from '../assets'
+import { navLinks } from '../constants'
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+         to="/"
+         className='flex items-center gap-2'
+         onClick={()=>{
+          setActive("");
+          window.scrollTo(0,0);
+         }}
+        >
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <p className='text-white font-bold cursor-pointer text-[18px]'>Salman<span className='sm:block hidden'>
+            | MERN Developer
+          </span>
+          </p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link)=>(
+            <li
+            key={link.id}
+            className={`${active===link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={()=>setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'> 
+            <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=>setToggle(!toggle)}/>
+            <div className={`${!toggle ? `hidden`: `flex`} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+            </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 

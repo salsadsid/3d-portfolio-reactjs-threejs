189626222e29ef6f469d881de0f4a96fc78a7405
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-09-22 16:10:57 +0600

    container-wrapper-framer-motion-animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-
+import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from "react-tilt";
+import { SectionWrapper } from "../hoc";
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          option={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 flex justify-evenly items-center flex-col min-h-[280px]"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-center text-white font-bold text-[20px]">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
@@ -28,8 +50,13 @@ const About = () => {
         updated with the latest technologies make me a valuable asset in the
         field.
       </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
